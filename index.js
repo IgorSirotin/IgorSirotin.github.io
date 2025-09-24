c2bd0bd1d5f77ef22ca0e6c71fff5a8bd263ade9
Сиротин Игорь <isirotin@softrust.ru>
2025-09-24 05:55:30 +0300

    ЗАменил на CDN

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Swiper from "./node_modules/swiper/swiper-bundle.mjs";
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.mjs';
 
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
